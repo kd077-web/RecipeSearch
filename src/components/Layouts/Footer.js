6ac97eef8669dfb897e0b8fbcb9ccedc6d00a4c5
kd077-web <96687394+kd077-web@users.noreply.github.com>
2024-02-16 12:33:22 +0545

    committtt

--- src/components/Layouts/Footer.js
+++ src/components/Layouts/Footer.js
@@ -1,11 +1,11 @@
 
-
+import {Link} from "react-router-dom";
  export const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white p-8">
       <div className="max-w-screen-xl mx-auto">
         <div className="text-center">
-          <h2 className="text-2xl font-bold">RecipeHub</h2>
+          <Link  to ="/ "><h2 className="text-2xl font-bold">RecipeHub</h2></Link>
           <p className="mt-2 text-sm">Your go-to ingredient-based recipe search engine.</p>
         </div>
         <div className="mt-8 text-sm text-gray-500">
